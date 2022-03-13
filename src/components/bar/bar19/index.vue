81429e38d0997400ac632f8a2d551d842bb20924
yekong <858834013@qq.com>
2022-03-13 10:33:07 +0800

    增加效果

---
+++ src/components/bar/bar19/index.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="echarts1" ref="echarts1">
+
+  </div>
+</template>
+
+<script>
+
+import { FontChart } from '@/utils/utils'
+
+export default {
+  name: 'echarts1',
+  components: {},
+  data () {
+    return {}
+  },
+  mounted () {
+    this.drawLine()
+  },
+  methods: {
+    drawLine () {
+      var that = this
+      window.addEventListener('resize', this.drawLine)
+      let myChart = this.$echarts.init(this.$refs.echarts1)
+      var option = {
+        legend: {
+          height: '60%',
+          show: true,
+          orient: 'vertical',
+          // orient: 'horizontal',
+          left: '10%',
+          icon: 'circle',
+          bottom: 'center',
+          align: 'left',
+          textStyle: {
+            color: '#fff',
+            fontSize: FontChart(14)
+          },
+          data: [{
+            name: '仓储租赁',
+            value: 10
+          }, {
+            name: 'null',
+            value: 20
+          }, {
+            name: '快递物流',
+            value: 30
+          }, {
+            name: '商贸市场',
+            value: 30
+          }, {
+            name: '冷链物流',
+            value: 30
+          }],
+          itemWidth: FontChart(15),
+          itemHeight: FontChart(10),
+          itemGap: FontChart(30)
+        },
+        series: [
+          {
+            type: 'pie',
+            center: ['70%', '50%'],
+            radius: ['70%', '85%'],
+            color: ['#00FFF6',
+              '#00E4FF',
+              '#3AFF7E',
+              '#00FFF6',
+              '#51EAC4'],
+            labelLine: {
+              normal: {
+                length: FontChart(20)
+              }
+            },
+            itemStyle: {
+              normal: {
+                borderColor: '#1F323F',
+                borderWidth: 3
+              }
+            },
+            label: {
+              normal: {
+                formatter: '{b|{b}} \n {per|({d}%)} ',
+                borderRadius: 4,
+                show: false,
+                rich: {
+                  b: {
+                    color: 'rgba(255, 255, 255, 1)',
+                    fontSize: FontChart(16)
+                  },
+                  per: {
+                    color: 'rgba(169, 177, 192, 1)',
+                    fontSize: FontChart(16),
+                    padding: [5, 0, 5, -5]
+                  }
+                },
+                textStyle: {
+                  color: '#fff',
+                  fontSize: FontChart(14)
+                }
+              }
+            },
+            data: [{
+              name: '仓储租赁',
+              value: 10
+            }, {
+              name: 'null',
+              value: 20
+            }, {
+              name: '快递物流',
+              value: 30
+            }, {
+              name: '商贸市场',
+              value: 30
+            }, {
+              name: '冷链物流',
+              value: 30
+            }]
+          }
+        ]
+      }
+      myChart.clear()
+      myChart.resize()
+      myChart.setOption(option)
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.echarts1 {
+  position: relative;
+  width: 100%;
+  height: calc(100% - 110px);
+}
+</style>
---
+++ src/components/zhu/zhu21/index.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="echarts1" ref="echarts">
+  </div>
+</template>
+
+<script>
+
+import { FontChart } from '@/utils/utils'
+
+export default {
+  name: 'echarts1',
+  components: {},
+  props: {
+    id: {
+      type: String,
+      default () {
+        return ''
+      }
+    }
+  },
+  data () {
+    return {}
+  },
+  watch: {},
+  mounted () {
+    this.drawLine()
+  },
+  methods: {
+    drawLine () {
+      var that = this
+      window.addEventListener('resize', this.drawLine)
+      let myChart = this.$echarts.init(that.$refs.echarts)
+      var option = {
+        grid: {
+          left: '5%',
+          right: '3%',
+          top: '15%',
+          bottom: '10%',
+          containLabel: true
+        },
+        tooltip: {
+          show: true,
+          trigger: 'axis',
+        },
+        xAxis: {
+          data: [
+            'null',
+            '仓储租赁',
+          ],
+          axisLine: {
+            lineStyle: {
+              color: 'rgba(94, 115, 152, 1)',
+            }
+          },
+          axisTick: {
+            show: false
+          },
+          axisLabel: {
+            color: 'rgba(146, 178, 215, 1)',
+            fontSize: FontChart(16),
+          }
+        },
+        yAxis: [
+          {
+            splitNumber: 3,
+            nameTextStyle: {
+              color: 'rgba(146, 178, 215, 1)',
+              fontSize: FontChart(16)
+            },
+            axisLine: {
+              show: false,
+              lineStyle: {
+                color: '#3d5269'
+              }
+            },
+            axisTick: {
+              show: false
+            },
+            axisLabel: {
+              color: 'rgba(146, 178, 215, 1)',
+              fontSize: FontChart(16),
+              formatter: function (value, index) {
+                let s = value + '亿'
+                return s
+              },
+            },
+            splitLine: {
+              show: true,
+              lineStyle: {
+                color: 'rgba(94, 115, 152, 1)'
+              }
+            },
+            yAxisIndex: 0
+          }],
+        series: [
+          {
+            name: '',
+            type: 'bar',
+            barWidth: 17,
+            barGap: '0%',
+            itemStyle: {
+              normal: {
+                barBorderRadius: [0, 32, 0, 0],
+                color: function (params) {
+                  return new that.$echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                    offset: 0,
+                    color: 'rgba(0, 255, 246, 1)'
+                  }, {
+                    offset: 1,
+                    color: 'rgba(0, 255, 246, 0.2)'
+                  }], false)
+                }
+              }
+            },
+            data: ['50', '75', '105', '130', '105', '130']
+          },
+        ]
+      }
+      myChart.clear()
+      myChart.resize()
+      myChart.setOption(option)
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.echarts1 {
+  position: relative;
+  width: 100%;
+  height: calc(100% - 60px);
+}
+</style>
