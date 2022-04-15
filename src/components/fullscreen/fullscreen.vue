3e6fda8768e2c3395933e6da9359f3006e94ceb0
yekong <858834013@qq.com>
2022-04-15 14:21:45 +0800

    全屏

---
+++ src/components/fullscreen/fullscreen.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="full" @click="quanping">
+    <img v-if="a % 2 == 0" src="../assets/full.png" alt="">
+    <span v-if="a % 2 == 0">全屏</span>
+    <img v-if="a % 2 == 1" src="../assets/icon_exit.png" alt="">
+    <span v-if="a % 2 == 1">退出</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "full",
+  components: {},
+  props: {
+    id: {
+      type: String,
+      default() {
+        return 'fullScreen';
+      }
+    }
+  },
+  data() {
+    return {
+      a: 0
+    }
+  },
+  watch: {},
+  mounted() {
+  },
+  methods: {
+    quanping() {
+      this.a++
+      this.a % 2 == 1 ? this.enterfullscreen() : this.exitfullscreen()
+    },
+    //控制全屏
+    enterfullscreen() { //进入全屏
+      var docElm = document.getElementById(this.id)
+      //W3C
+      if (docElm.requestFullscreen) {
+        docElm.requestFullscreen()
+      }
+      //FireFox
+      else if (docElm.mozRequestFullScreen) {
+        docElm.mozRequestFullScreen()
+      }
+      //Chrome等
+      else if (docElm.webkitRequestFullScreen) {
+        docElm.webkitRequestFullScreen()
+      }
+      //IE11
+      else if (docElm.msRequestFullscreen) {
+        docElm.msRequestFullscreen()
+      }
+    },
+    //退出全屏
+    exitfullscreen() {
+      if (document.exitFullscreen) {
+        document.exitFullscreen()
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen()
+      } else if (document.webkitCancelFullScreen) {
+        document.webkitCancelFullScreen()
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen()
+      }
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.full {
+  width: 140px;
+  height: 60px;
+  cursor: pointer;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: nowrap;
+  flex-direction: row;
+  align-content: flex-start;
+  position: absolute;
+  z-index: 1000;
+  right: 20px;
+  bottom: 20px;
+
+  img {
+    width: 30px;
+    margin-right: 20px;
+  }
+
+  span {
+    font-size: 26px;
+    font-family: PingFangSC-Medium, PingFang SC;
+    font-weight: 500;
+    color: #FFFFFF;
+  }
+}
+</style>
