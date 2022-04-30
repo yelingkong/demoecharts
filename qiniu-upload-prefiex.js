f7939fa54337726cfb6ce2e6e18b808f6afd5871
yekong <858834013@qq.com>
2022-04-30 09:07:19 +0800

    更新

---
+++ qiniu-upload-prefiex.js
@@ -0,0 +1,7 @@
+module.exports = {
+  prefix: 'demoecharts/', // 七牛目录
+  prefixhost: 'https://images.wanjunshijie.com/',  //七牛cdn绑定的域名
+  accessKey: '', //七牛accessKey
+  secretKey: '', //七牛secretKey
+  space: '', //七牛空间名
+}
