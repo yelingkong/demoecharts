5f83705440e65b71d49fbfe17c514ef6daa1b9df
yekong <858834013@qq.com>
2022-07-02 15:30:52 +0800

    update .gitignore

--- qiniu-upload-prefiex.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  prefix: 'demoecharts/', // 七牛目录
-  prefixhost: 'https://images.wanjunshijie.com/',  //七牛cdn绑定的域名
-  accessKey: 'X0qG-ShIPPpHG3YtD01WfnbNdB6PlG0ZRt_mxYEm', //七牛accessKey
-  secretKey: 'N-Cp2ScgBJvvecg0gxzWA_uqYqmFvY-xTy-huZH1', //七牛secretKey
-  space: 'wanjun', //七牛空间名
-}
